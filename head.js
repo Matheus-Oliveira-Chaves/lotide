1b6dddbc23f960dd8a536f0a1f23bfbac87b1757
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-07-17 21:54:47 -0400

    lotide test

---
+++ head.js
@@ -0,0 +1,14 @@
+const assertEqual = function (actual, expected) {
+ 
+};
+function head(assertEqual) {
+  if (assertEqual && assertEqual.length > 0) {
+    console.log( assertEqual[0]);
+    return
+  }
+  console.log( undefined);
+}
+
+assertEqual(head([5,5,5]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+

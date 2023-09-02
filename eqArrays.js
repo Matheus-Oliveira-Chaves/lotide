57b117a6e1e4600d1177c76c084117c23e62f6da
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-09-01 21:12:31 -0400

    add arrays have diff length

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,9 @@
 const assertEqual = require (`./assertEqual`)
 
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false;
+  }
   // Iterate through the arrays and compare each element
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {

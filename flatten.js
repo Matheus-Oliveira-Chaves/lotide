4d91c63972bae1231d80c6eab4c1d5c1e5f39bf9
Matheus-Oliveira-Chaves <Matheus.oliveira.chaves@hotmail.com>
2023-09-01 21:17:10 -0400

    update to deal w/ nested arrays

--- flatten.js
+++ flatten.js
@@ -1,11 +1,10 @@
 function flatten(arr) {
   const flattenedArray = [];
-  // start w/ empty array
+  
   for (let i = 0; i < arr.length; i++) {
-    //for loop through array
     if (Array.isArray(arr[i])) {
       // If the element is an array, concatenate its elements to the flattenedArray
-      flattenedArray.push(...arr[i]);
+      flattenedArray.push(...flatten(arr[i]));
     } else {
       // If the element is not an array, simply push it to the flattenedArray
       flattenedArray.push(arr[i]);
@@ -14,3 +13,5 @@ function flatten(arr) {
 
   return flattenedArray;
 }
+
+module.exports = flatten;
